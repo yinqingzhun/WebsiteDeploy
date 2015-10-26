cc12946ac21f868a585d7253643b83c4fd95d865
yqz <103025600@qq.com>
2015-10-26 22:57:12 +0800

    增加复制工具类

--- AohPackageSubscriber/DeployMonitorService.cs
+++ AohPackageSubscriber/DeployMonitorService.cs
@@ -2,20 +2,42 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using System.Threading;
 namespace AohPackageSubscriber
 {
     public class DeployMonitorService:IDaemonService
     {
-
+        private const int interval = 3000;
         public void Start()
         {
-            throw new NotImplementedException();
+            Thread t = new Thread(MonitorNewDeploy);
+            t.Start();
         }
 
         public void Stop()
         {
-            throw new NotImplementedException();
+            
+        }
+
+        private void MonitorNewDeploy()
+        {
+            while (true)
+            {
+                //获取最新可用包文件
+
+                //解压文件
+
+                //备份文件1，2
+
+                //复制文件
+
+                //失败时，回滚到备份文件
+
+                //回滚成功时，覆盖文件
+
+                Thread.Sleep(interval);
+            }
+
         }
     }
 }
---
+++ Utils/XCopy.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace WebDeploy.Utils
+{
+    public class XCopy
+    {
+        /// <summary>
+        /// 复制文件到指定目录
+        /// </summary>
+        /// <param name="srcDir">原文件目录</param>
+        /// <param name="dstDir">存放目录</param>
+        /// <returns>true</returns>
+        public bool Copy(String srcDir, String dstDir)
+        {
+            if (!File.Exists(srcDir))
+                return false;
+
+            if (!File.Exists(dstDir))
+                File.Create(dstDir);
+
+            string cmd = string.Format("xcopy {0} {1} /i/e/v/Y/D", srcDir, dstDir);
+            System.Diagnostics.Process p = new System.Diagnostics.Process();
+            p.StartInfo.FileName = "cmd.exe";
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardInput = true;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.CreateNoWindow = true;
+            p.Start();
+            string sOutput = p.StandardOutput.ReadToEnd();
+            Console.WriteLine(sOutput);
+            return true;
+        }
+    }
+}
